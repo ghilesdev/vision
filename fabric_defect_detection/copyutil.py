39fe0e3debcf519f2ab2896fca1dbdb6d7bede1b
aghiles belkessam <ghilesauto@gmail.com>
2020-03-10 17:32:00 +0100

    black

--- fabric_defect_detection/copyutil.py
+++ fabric_defect_detection/copyutil.py
@@ -1,12 +1,13 @@
 import os
 import shutil
 from pathlib import Path
+
 """
     copies all files in subdir into dir and renames them
 """
-dest='dataset\\nodefects'
-count =0
-for src, dir, files in os.walk('dataset\\noDefect'):
+dest = "dataset\\nodefects"
+count = 0
+for src, dir, files in os.walk("dataset\\noDefect"):
     print(src)
 
     for file in files:
@@ -15,10 +16,10 @@ for src, dir, files in os.walk('dataset\\noDefect'):
         # if os.path.isfile(path):
         #     print(dest+path)
 
-        count+=1
+        count += 1
         # print(paths)
-        new_path=shutil.copy(path, dest)
-        p=Path(new_path)
+        new_path = shutil.copy(path, dest)
+        p = Path(new_path)
         p.rename(Path(p.parent, f"{p.stem}_{count}_{p.suffix}"))
         # print('new path', new_path)
         # shutil.move(new_path,  new_path[-1]+str(count))
